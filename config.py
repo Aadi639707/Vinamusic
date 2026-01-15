38808bc35c73103d03e56b8163eb9150e0dc413a
Aadi639707 <aadi639707@gmail.com>
2026-01-15 22:59:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,19 @@
+import os
+
+# Telegram API Details
 API_ID = 31633374
 API_HASH = "f00c26a410219039503486c6a115a5e6"
+
+# Bot & Session Details
 BOT_TOKEN = "8507364415:AAFtrcpi2lZ9YarfFZAjcF37W3Y5wNfKWV0"
-MONGO_DB_URI = "mongodb+srv://vina:UIV8FRsRHedekmeJ@cluster0.aoxubn3.mongodb.net/?appName=Cluster0"
 STRING_SESSION = "BQHir94AWkp7BUVZEkAE7oOVZ7zboXz9A0Oi38-bP413TZ0xZvMsmapkaWt3lmrMrcYVBBnnY31PzYCKrhghnsbrXBuAL7XKKY-jyDJSqfxG2s9E-v8TzVa1DclsRzko1fAssh-syXbits94cOVy6FOJWewfjvHuNES7p5RT_4W4AYJpYRc-rpeQoC1LO6JZWER1yhlVBDJ_xhh7cUVZhvjAxtmZ5YJMVhBV226BHwehG4hZ9mscu0mzJzhzw678-7R32j3B3Yvdoz7x5HbaZCloj2q1bb0brHLB1_OfdJBt6VpSSt7gxlHT3X1BfIg1N_mUVWyN96ZWYnG5YuqGpesjWGYQcAAAAAHcRyetAA"
+
+# Database & Logs
+MONGO_DB_URI = "mongodb+srv://vina:UIV8FRsRHedekmeJ@cluster0.aoxubn3.mongodb.net/?appName=Cluster0"
+LOG_GROUP_ID = -1003505506674
 OWNER_ID = 8292600776
-START_IMG = "https://telegra.ph/file/your_image_link.jpg" # Start image link
+
+# Social Links & Images
+SUPPORT_CHAT = "https://t.me/MusicSupportGcc"
+UPDATE_CHANNEL = "https://t.me/vinaupdatee"
+START_IMG = "https://graph.org/file/f05e5d3269b618f0f0031.jpg"
