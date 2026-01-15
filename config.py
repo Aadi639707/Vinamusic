c41f76d35bd9ab8c550658d3fd7ef89f2136bdb8
Aadi639707 <aadi639707@gmail.com>
2026-01-15 22:38:40 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+API_ID = 31633374
+API_HASH = "f00c26a410219039503486c6a115a5e6"
+BOT_TOKEN = "8507364415:AAFtrcpi2lZ9YarfFZAjcF37W3Y5wNfKWV0"
+MONGO_DB_URI = "mongodb+srv://vina:UIV8FRsRHedekmeJ@cluster0.aoxubn3.mongodb.net/?appName=Cluster0"
+STRING_SESSION = "BQHir94AWkp7BUVZEkAE7oOVZ7zboXz9A0Oi38-bP413TZ0xZvMsmapkaWt3lmrMrcYVBBnnY31PzYCKrhghnsbrXBuAL7XKKY-jyDJSqfxG2s9E-v8TzVa1DclsRzko1fAssh-syXbits94cOVy6FOJWewfjvHuNES7p5RT_4W4AYJpYRc-rpeQoC1LO6JZWER1yhlVBDJ_xhh7cUVZhvjAxtmZ5YJMVhBV226BHwehG4hZ9mscu0mzJzhzw678-7R32j3B3Yvdoz7x5HbaZCloj2q1bb0brHLB1_OfdJBt6VpSSt7gxlHT3X1BfIg1N_mUVWyN96ZWYnG5YuqGpesjWGYQcAAAAAHcRyetAA"
+OWNER_ID = 8292600776
+START_IMG = "https://telegra.ph/file/your_image_link.jpg" # Start image link
